dc9f3a9c816b2ab22f68da13e8934bb0aec03a94
mixedbyinstinct <92763310+mixedbyinstinct@users.noreply.github.com>
2022-10-10 21:42:02 -0400

    Add files via upload

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,31 @@
 import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import tychoFace from './IMG_2489.jpg';
+import tychoFace from './Tycho_update_photo.jpg';
 import About from './components/about.jsx';
 import Art from './components/art.jsx';
-import { Container, Header, Menu, Button, Jumbotron, GlobalStyle, Text, Separator, ArtHeader } from './styles/App.styled';
+import { Container, ScrollContainer, Header, Menu, Button, Jumbotron, GlobalStyle, Text, Separator, ArtHeader, MenuButton } from './styles/App.styled';
 import axios from 'axios';
+import Socials from './components/socials.jsx';
 
 const App = () => {
   const bioRef = useRef(null);
   const newsRef = useRef(null);
   const artRef = useRef(null);
+  const socialsRef = useRef(null);
   
   const [showMenu, setShowMenu] = useState(false);
   const [showBio, setShowBio] = useState(false);
   const [showArt, setShowArt] = useState(false);
-  const [newsText, setNewsText] = useState('');
-  
+  const [newsData, setNewsData] = useState([]);
+  const [debugText, setDebugText] = useState('');
+  const [showSocials, setShowSocials] = useState(false);
   
     
  const MenuScreen = () => {
   return (
     <Menu>
     <Button onClick={toggleBio}>About</Button>
-    <Button>Socials</Button>
+    <Button onClick={toggleSocials}>Socials</Button>
     <Button onClick={toggleNews}>News</Button>
     <Button onClick={toggleArt}>Music</Button>
     </Menu>
@@ -34,6 +37,13 @@ const App = () => {
       setShowMenu(false);
       bioRef.current.scrollIntoView();
     }
+    
+    async function toggleSocials(){
+      await setShowSocials(true);
+      setShowMenu(false);
+      socialsRef.current.scrollIntoView();
+    }
+    
     
     async function toggleNews() {
       await setShowMenu(false);
@@ -50,10 +60,22 @@ const App = () => {
     setShowMenu(!showMenu);
   }
   
+  const renderNewsData = () => {
+    return newsData.map((item, index) => {
+      const { Id, Headline, Content } = item;
+      return (
+               <div key={Id}>
+                 <h2>{Headline}</h2>
+                 <p>{Content}</p>
+               </div>
+             );
+    })
+  }
+  
   useEffect(() => {
-    // TODO: news specific API route
-    axios.get('/bio').then(({data}) => {
-      setNewsText(data)
+    axios.get('/news').then(({data}) => {
+      data.success ? setNewsData(data.newsData) :
+      setDebugText(data.message);
     })
   }, [])
   
@@ -61,32 +83,40 @@ const App = () => {
     <>
     <GlobalStyle />
     <Container>
-    {showMenu ? <MenuScreen /> : <div />}
-    <Jumbotron>
-    <h1>Tycho</h1>
-    <img src={tychoFace} alt ="tycho" />
-    </Jumbotron>
-    <Header>
-    <p>Menu</p>
-    <Button onClick={toggleMenu}>Click</Button>
-    </Header>
-    <Container ref={newsRef}>
-    <h2>News</h2>
-    <Text>
-    <p>{newsText}</p>
-    </Text>
-    <Separator />
-    </Container>
-    <Container ref={bioRef}>
-    {showBio ? <About /> : <div />}
-    {showBio ? <Separator /> : <div />}
-    </Container>
-    <div ref={artRef}>
-    {showArt ? <ArtHeader>
-    <h2>Music</h2>
-    <Art />
-    </ArtHeader> : <div />}
-    </div>
+      {showMenu ? <MenuScreen /> : <div />}
+      <Jumbotron onClick={() => setShowMenu(false)}>
+        <h1>Tycho</h1>
+        <img src={tychoFace} alt ="tycho" />
+      </Jumbotron>
+      <Header>
+        <Button onClick={toggleMenu}> Main Menu</Button>
+      </Header>
+      <h2>News</h2>
+      <ScrollContainer ref={newsRef} onClick={() => setShowMenu(false)}>
+        <Text>
+          {renderNewsData()}
+        </Text>
+        <Separator />
+      </ScrollContainer>
+      <h2>Bio</h2>
+      <ScrollContainer ref={bioRef} onClick={() => setShowMenu(false)}>
+        {showBio ? <About /> : <div />}
+        {showBio ? <Separator /> : <div />}
+      </ScrollContainer>
+      <div ref={artRef}>
+        {showArt ? <ArtHeader>
+          <h2>Music</h2>
+          <Art />
+        </ArtHeader> : <div />}
+      </div>
+      <div ref={socialsRef}>
+        {showSocials ?
+          <>
+          <h2>Social Media</h2>
+          <Socials />
+          </> : <div />}
+        </div>
+      <p>{debugText}</p>
     </Container>
     </>
   )
